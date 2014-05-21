28b0a67fa0840886e22c7370003d701b7fa7891c
Darren Burgess <darrentburgess@gmail.com>
2014-05-20 20:30:08 -0400

    commit

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 (function($){
-$(document).ready(function(){
+$(document).ready(function() {
 
 var escapeKey = 27;	
 	
@@ -8,7 +8,7 @@ $(".what").click(function(){
 	$(".overlay").fadeIn(1000);
 });
 
-// Hide information modal box
+// Hide information modal box - button or escape key
 $("a.close").click(function(){
 	$(".overlay").fadeOut(1000);
 });
@@ -19,13 +19,30 @@ $(document).keyup(function(e){
 	}
 });
 
-// start new game on page load.  set feed back to 'make your guess'
-
-// start new game on new game button click
-
 // function: generate random number
+var randomNumber = function(){
+	return Math.floor((Math.random() * 100) + 1);
+};
+
+var correctAnswer = Math.floor((Math.random() * 100) + 1);
+
+// start new game on button click.  set feed back to 'make your guess'
+$('.new').on('click', function (){
+	$('form').trigger("reset");
+	$('#feedback').text('Make your Guess!');
+	$('#count').text(countElements('#guestList > li')); //TEST
+	correctAnswer = randomNumber();//TEST
+	console.log(correctAnswer);
+});
+
+// function: count elements 
+var countElements = function(selector){
+	return $(selector).length; //TEST
+};
 
 // function: evaluate answer
+var evaluateAnswer = function(answer){
+};
 
 // capture answer with return
 
